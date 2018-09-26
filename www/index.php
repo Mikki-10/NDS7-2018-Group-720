1dd9d19417e87d88666fdc97e1ab1ad68b53bdb3
Mikki Sørensen <Mikki-10@hotmail.com>
2018-09-26 12:37:01 +0200

    new curl code

--- www/index.php
+++ www/index.php
@@ -1,19 +1,23 @@
 <?php
 
-echo "Hallo World";
+echo "Hallo World<br>";
 
-$data_string = '{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":74}';
+$ch = curl_init();
 
-$ch = curl_init('172.18.0.3:8545');                                                                      
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',                                                                                
-    'Content-Length: ' . strlen($data_string))                                                                       
-);                                                                                                                   
-                                                                                                                     
+curl_setopt($ch, CURLOPT_URL, "172.18.0.3:8545");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"jsonrpc\":\"2.0\",\"method\":\"admin_peers\",\"params\":[],\"id\":74}");
+curl_setopt($ch, CURLOPT_POST, 1);
+
+$headers = array();
+$headers[] = "Content-Type: application/json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
 $result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
 
 echo "<pre>"; var_dump($result); echo "</pre>";
 

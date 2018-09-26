f8ab6ba6962fb35b3d4c83554107f860dbf811c5
Mikki Sørensen <Mikki-10@hotmail.com>
2018-09-26 12:48:01 +0200

    trying auto build of array to table

--- www/index.php
+++ www/index.php
@@ -37,4 +37,34 @@ catch (Exception $e)
 }
 
 
+echo build_table($result);
+
+
+function build_table($array)
+{
+    // start table
+    $html = '<table>';
+    // header row
+    $html .= '<tr>';
+    foreach($array[0] as $key=>$value){
+            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+        }
+    $html .= '</tr>';
+
+    // data rows
+    foreach( $array as $key=>$value){
+        $html .= '<tr>';
+        foreach($value as $key2=>$value2){
+            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+        }
+        $html .= '</tr>';
+    }
+
+    // finish table and return it
+
+    $html .= '</table>';
+    return $html;
+}
+
+
 ?>

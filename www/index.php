8594b6c64543c1874ab88c519d6354e282346626
Mikki Sørensen <Mikki-10@hotmail.com>
2018-09-26 12:39:28 +0200

    something is not right doing a try catch

--- www/index.php
+++ www/index.php
@@ -2,26 +2,34 @@
 
 echo "Hallo World<br>";
 
-$ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "172.18.0.3:8545");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"jsonrpc\":\"2.0\",\"method\":\"admin_peers\",\"params\":[],\"id\":74}");
-curl_setopt($ch, CURLOPT_POST, 1);
+try 
+{
+	#curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":74}' 172.18.0.3:8545
+	
+	$ch = curl_init();
 
-$headers = array();
-$headers[] = "Content-Type: application/json";
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_URL, "172.18.0.3:8545");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"jsonrpc\":\"2.0\",\"method\":\"admin_peers\",\"params\":[],\"id\":74}");
+	curl_setopt($ch, CURLOPT_POST, 1);
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
+	$headers = array();
+	$headers[] = "Content-Type: application/json";
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+	$result = curl_exec($ch);
+	if (curl_errno($ch)) {
+	    echo 'Error:' . curl_error($ch);
+	}
+	curl_close ($ch);
+
+	echo "<pre>"; var_dump($result); echo "</pre>";
+} 
+catch (Exception $e) 
+{
+	echo "<pre>"; var_dump($e); echo "</pre>";
 }
-curl_close ($ch);
 
-echo "<pre>"; var_dump($result); echo "</pre>";
-
-
-#curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":74}' 172.18.0.3:8545
 
 ?>

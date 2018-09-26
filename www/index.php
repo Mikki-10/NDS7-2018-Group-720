d3bb351f07ad1dc6c767b54bbd436a7478877970
Mikki Sørensen <Mikki-10@hotmail.com>
2018-09-26 13:49:26 +0200

    making it to af function

--- www/index.php
+++ www/index.php
@@ -24,47 +24,53 @@ table, th, td {
 
 echo "Hallo World<br>";
 
+$ip = "172.18.0.3:8545";
+$request = '{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":74}';
 
-try 
+make_rpc_request($ip, $request);
+
+function make_rpc_request($ip, $request)
 {
-	#curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":74}' 172.18.0.3:8545
+	try 
+	{
+		#curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":74}' 172.18.0.3:8545
 
-	$ch = curl_init();
+		$ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, "172.18.0.3:8545");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"jsonrpc\":\"2.0\",\"method\":\"admin_peers\",\"params\":[],\"id\":74}");
-	curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_URL, $ip);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+		curl_setopt($ch, CURLOPT_POST, 1);
 
-	$headers = array();
-	$headers[] = "Content-Type: application/json";
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$headers = array();
+		$headers[] = "Content-Type: application/json";
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-	$result = curl_exec($ch);
-	if (curl_errno($ch)) {
-	    echo 'Error:' . curl_error($ch);
+		$result = curl_exec($ch);
+		if (curl_errno($ch)) {
+		    echo 'Error:' . curl_error($ch);
+		}
+		curl_close ($ch);
+
+		$result = json_decode($result, true);
+
+		//echo "<pre>"; var_dump($result); echo "</pre>";
+	} 
+	catch (Exception $e) 
+	{
+		echo "<pre>"; var_dump($e); echo "</pre>";
 	}
-	curl_close ($ch);
 
-	$result = json_decode($result, true);
 
-	echo "<pre>"; var_dump($result); echo "</pre>";
-} 
-catch (Exception $e) 
-{
-	echo "<pre>"; var_dump($e); echo "</pre>";
+
+	foreach ($result["result"] as $key => $value) 
+	{
+		echo build_table($value);
+		echo "<br>";
+	}
 }
 
 
-
-foreach ($result["result"] as $key => $value) 
-{
-	echo build_table($value);
-	echo "<br>";
-}
-
-
-echo "<body>";
 
 function build_table($array)
 {

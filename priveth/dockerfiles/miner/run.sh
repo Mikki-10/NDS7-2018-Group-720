d643461dfba4e2be2dff8e123893baeb5748625f
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-10-11 10:17:29 +0200

    Turns out we have to log by redirecting stderr. Changed so that miners creates logfiles with their own names prefixed to the file. ie miner1_log.txt. This means all miners can log to the same folder. Made verbosity settable by env variable VERBOSITY. Currently only active when env LOGGING is "on"

--- priveth/dockerfiles/miner/run.sh
+++ priveth/dockerfiles/miner/run.sh
@@ -9,12 +9,12 @@ then
         --rpcapi="db,eth,net,web3,personal,miner,admin" \
         --datadir=/gethdata \
         --networkid=44686 \
-        --verbosity 4 \
+        --verbosity $VERBOSITY \
         --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.18.0.2:30301 \
         --mine \
         --minerthreads=1 \
         --etherbase=0x0000000000000000000000000000000000000001 \
-        >> /logpath/log.txt
+        2>> /logpath/${DOCKER_NAME}_log.txt
 else
     geth \
         --identity $DOCKER_NAME \

1d482fda9852c58a48a89127c6229c34240d9dd5
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-10-13 20:51:08 +0200

    Working least squares trilateration. Might even be multilateration.

--- MiniProject_CNAI/trilaterate.m
+++ MiniProject_CNAI/trilaterate.m
@@ -1,12 +1,18 @@
 function loc = trilaterate(towerPos, distance)
+    %(Pi - P0)' * (Pi - P0) = ri^2
+    
+    % Offset coordinates so that first tower is at origin.
+    offset = towerPos(1, :);
+    offsetTower = towerPos - offset;
     
     % construct Hx=b
-    H = towerPos(2:end, 1:end);
+    H = offsetTower(2:end, 1:end);
     
     ksqr = sum(H.^2, 2);
     
     b = 1/2 * [ksqr - distance(2:end, 1:end).^2 + distance(1, 1:end).^2 ];
     
-    loc = H\b;
+    % Solve least square equation and add offset back.
+    loc = (H\b) + offset';
     
 end

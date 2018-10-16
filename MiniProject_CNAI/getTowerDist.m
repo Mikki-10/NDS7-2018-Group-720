9b3c3b1917b875c837791ad2901176d87c171e08
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-10-16 19:37:37 +0200

    Some comments on the distance function.

--- MiniProject_CNAI/getTowerDist.m
+++ MiniProject_CNAI/getTowerDist.m
@@ -2,6 +2,14 @@
 % If third argument is 1, gaussian noise is added to the distances.
 function d = getTowerDist(towerCoords, walkerCoords, addNoise)
     d = [];
+    
+    % For each tower. Take its [X;Y] coordinates and subtract it from all
+    % walker coordinates. This creates a collection of vectors between a
+    % tower and each walker coordinate. This forms a 2xn matrix of vectors.
+    % Caluclate the distance of each vector by calculating the two norm ie
+    % eucledian distance.
+    % Append the distance to d forming a Nx3 matrix where the rows are the
+    % towers, and the colums distances between the walker and tower
     for t = towerCoords'
         towerDistVec = t - walkerCoords';
         d = [d; vecnorm(towerDistVec)];

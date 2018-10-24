762a3b135fab3ed39767eb888c5fd370be9dc4a9
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-10-24 13:13:36 +0200

    Make block chain data persistent. It is now stored in ./gethdata and each node and miner will create its own subdir. Make the regular nodes log.

--- priveth/dockerfiles/miner/run.sh
+++ priveth/dockerfiles/miner/run.sh
@@ -1,5 +1,6 @@
 if [ $LOGGING == "on" ]
 then
+    mkdir /gethdata/${DOCKER_NAME}
     geth \
         --identity $DOCKER_NAME \
         --rpcvhosts=* \
@@ -7,15 +8,16 @@ then
         --rpcaddr $(hostname -i) \
         --ethstats $DOCKER_NAME:nDs2018@webstats-frontend:3000 \
         --rpcapi="db,eth,net,web3,personal,miner,admin" \
-        --datadir=/gethdata \
+        --datadir=/gethdata/${DOCKER_NAME} \
         --networkid=44686 \
         --verbosity $VERBOSITY \
         --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.18.0.2:30301 \
         --mine \
         --minerthreads=1 \
         --etherbase=0x0000000000000000000000000000000000000001 \
-        2>> /logpath/${DOCKER_NAME}_log.txt
+        2> /logpath/${DOCKER_NAME}_log.txt
 else
+    mkdir /gethdata/${DOCKER_NAME}
     geth \
         --identity $DOCKER_NAME \
         --rpcvhosts=* \
@@ -23,7 +25,7 @@ else
         --rpcaddr $(hostname -i) \
         --ethstats $DOCKER_NAME:nDs2018@webstats-frontend:3000 \
         --rpcapi="db,eth,net,web3,personal,miner,admin" \
-        --datadir=/gethdata \
+        --datadir=/gethdata/${DOCKER_NAME} \
         --networkid=44686 \
         --verbosity 4 \
         --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.18.0.2:30301 \

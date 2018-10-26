b60706d094e5fe7a21f5a1bd60521f24d13421de
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-10-26 11:40:59 +0200

    test without npm

--- priveth/dockerfiles/explorer/run.sh
+++ priveth/dockerfiles/explorer/run.sh
@@ -18,5 +18,4 @@ geth \
     --networkid=44686 \
     --verbosity 4 \
     --rpccorsdomain "http://${hostname -i}:8000" \
-    --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.18.0.2:30301 \ 
-    & npm start
+    --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.18.0.2:30301

48522d224e82e332a0f1a558b0a8a6f5fd9c0f5c
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-10-31 09:07:38 +0100

    Remove rcpcorsdomain as a test.

--- priveth/dockerfiles/node/run.sh
+++ priveth/dockerfiles/node/run.sh
@@ -16,7 +16,7 @@ then
         --datadir=/gethdata/${DOCKER_NAME} \
         --networkid=44686 \
         --verbosity $VERBOSITY \
-        --rpccorsdomain "http://$(hostname -i):8000"
+        #--rpccorsdomain "http://$(hostname -i):8000"
         --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.18.0.2:30301 \
         2> /logpath/${DOCKER_NAME}_log.txt
 else
@@ -27,7 +27,7 @@ else
         --rpcaddr $(hostname -i) \
         --ethstats $DOCKER_NAME:nDs2018@webstats-frontend:3000 \
         --rpcapi="db,eth,net,web3,personal,miner,admin" \
-        --datadir=/gethdata/${DOCKER_NAME} \
+        #--datadir=/gethdata/${DOCKER_NAME} \
         --networkid=44686 \
         --verbosity 4 \
         --rpccorsdomain "http://$(hostname -i):8000"

af3fad28f1b84e59a0230212ecf758b45147288b
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-10-31 14:57:10 +0100

    fix durp

--- priveth/dockerfiles/node/run.sh
+++ priveth/dockerfiles/node/run.sh
@@ -15,6 +15,7 @@ then
         --rpcapi="db,eth,net,web3,personal,miner,admin" \
         --datadir=/gethdata/${DOCKER_NAME} \
         --networkid=44686 \
+        --rpccorsdomain "http://$(hostname -i):8000" \
         --verbosity $VERBOSITY \
         --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.18.0.2:30301 \
         2> /logpath/${DOCKER_NAME}_log.txt
@@ -28,6 +29,6 @@ else
         --rpcapi="db,eth,net,web3,personal,miner,admin" \
         --networkid=44686 \
         --verbosity 4 \
-        --rpccorsdomain "http://$(hostname -i):8000"
+        --rpccorsdomain "http://$(hostname -i):8000" \
         --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.18.0.2:30301
 fi

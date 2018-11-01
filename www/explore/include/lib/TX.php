a6859a71fe234d1f55bd17e3ef24c75180e45142
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-01 14:53:45 +0100

    starting on TX

--- www/explore/include/lib/TX.php
+++ www/explore/include/lib/TX.php
@@ -26,6 +26,40 @@ class TX
 		$tx_data = $tx_data["result"];
 		
 		echo "<pre>"; var_dump($tx_data); echo "</pre>";
+
+		echo '<div class="container"><br>';
+
+		echo '<div class="row"><div class="col-md-1">';
+
+		echo '</div><div class="col-md-10 text-center">';
+		echo "<h1> $tx_data["hash"] </h1>";
+		echo '</div><div class="col-md-1">';
+
+		echo "</div></div>";
+
+		?>
+		<div class="table-responsive">
+		<table class="table table-hover">
+		<tbody>
+			<tr><td>Hash</td><td><?php echo $tx_data["hash"]; ?></td></tr>
+			<tr><td>blockHash</td><td><a href="?block=<?php echo $tx_data["blockHash"] ?>"><?php echo $tx_data["blockHash"]; ?></a></td></tr>
+			<tr><td>blockNumber</td><td><a href="?account=<?php echo hexdec($tx_data["blockNumber"]); ?>"><?php echo hexdec($tx_data["blockNumber"]); ?></a></td></tr>
+			<tr><td>from</td><td><?php echo $tx_data["from"]; ?></td></tr>
+			<tr><td>to</td><td><?php echo $tx_data["to"]; ?></td></tr>
+			<tr><td>value</td><td><?php echo hexdec($tx_data["value"]); ?></td></tr>
+			<tr><td>nonce</td><td><?php echo hexdec($tx_data["nonce"]); ?></td></tr>
+			<tr><td>gas limit</td><td><?php echo "get this value"; ?></td></tr>
+			<tr><td>gas</td><td><?php echo hexdec($tx_data["gas"]); ?></td></tr>
+			<tr><td>gasPrice</td><td><?php echo hexdec($tx_data["gasPrice"]); ?></td></tr>
+			<tr><td>fee</td><td><?php echo "get this value"; ?></td></tr>
+			<tr><td>data (input)</td><td><?php echo $tx_data["input"]; ?></td></tr>
+			<tr><td>v</td><td><?php echo $tx_data["v"]; ?></td></tr>
+			<tr><td>r</td><td><?php echo $tx_data["r"]; ?></td></tr>
+			<tr><td>s</td><td><?php echo $tx_data["s"]; ?></td></tr>
+		</tbody>
+		</table>
+		</div>
+		</div>
 	}
 }
 

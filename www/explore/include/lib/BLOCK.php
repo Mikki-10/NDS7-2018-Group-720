61799382d48c67394c0b8c619b691b0e9ff43e88
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-01 12:19:01 +0100

    Transactions value fix

--- www/explore/include/lib/BLOCK.php
+++ www/explore/include/lib/BLOCK.php
@@ -83,7 +83,7 @@ class BLOCK
 			echo "<td>" . $value["hash"] . "</td>";
 			echo "<td>" . $value["from"] . "</td>";
 			echo "<td>" . $value["to"] . "</td>";
-			echo "<td>" . hexdec($value["value"]) . "</td>";
+			echo "<td>" . hexdec($value["value"])/100000000000000000 . "</td>";
 			echo "</tr>";
 		}
 		echo "</tbody>";

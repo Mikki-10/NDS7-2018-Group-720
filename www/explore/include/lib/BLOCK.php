63aefd45fbd07c1fd5a4facb4b7911bd5dbeaa81
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-01 13:34:31 +0100

    syntax

--- www/explore/include/lib/BLOCK.php
+++ www/explore/include/lib/BLOCK.php
@@ -80,7 +80,7 @@ class BLOCK
 		foreach ($block_data["transactions"] as $key => $value) 
 		{
 			echo "<tr>";
-			echo '<td><a href="?tx=' . $value["hash"]) . '">' . $value["hash"] . '</a></td>';
+			echo '<td><a href="?tx=' . $value["hash"] . '">' . $value["hash"] . '</a></td>';
 			echo '<td><a href="?account=' . $value["from"] . '">' . $value["from"] . '</a></td>';
 			echo '<td><a href="?account=' . $value["to"] . '">' . $value["to"] . '</a></td>';
 			echo '<td>' . number_format(hexdec($value["value"])/1000000000000000000, 18, ",", ".") . '</td>';
--- www/explore/include/lib/FRONTPAGE.php
+++ www/explore/include/lib/FRONTPAGE.php
@@ -82,7 +82,7 @@ class FRONTPAGE
 			foreach ($block["result"]["transactions"] as $key => $value) 
 			{
 				echo "<tr>";
-				echo '<td><a href="?tx=' . $value["hash"]) . '">' . $value["hash"] . '</a></td>';
+				echo '<td><a href="?tx=' . $value["hash"] . '">' . $value["hash"] . '</a></td>';
 				echo '<td><a href="?account=' . $value["from"] . '">' . $value["from"] . '</a></td>';
 				echo '<td><a href="?account=' . $value["to"] . '">' . $value["to"] . '</a></td>';
 				echo '<td>' . number_format(hexdec($value["value"])/1000000000000000000, 18, ",", ".") . '</td>';

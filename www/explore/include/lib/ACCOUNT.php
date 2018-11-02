2862dc1b1d454a5b17550b51127cd402cbb512cb
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-02 12:28:23 +0100

    addting account data

--- www/explore/include/lib/ACCOUNT.php
+++ www/explore/include/lib/ACCOUNT.php
@@ -21,17 +21,38 @@ class ACCOUNT
 		$RPC = new RPC();
 		$account_data = $RPC->get_Account_Balance($account);
 
-		echo "<pre>"; var_dump($account_data); echo "</pre>";
+		//echo "<pre>"; var_dump($account_data); echo "</pre>";
 
 		foreach ($account_data as $key => $value) 
 		{
 			$account_info[$key] = rtrim(rtrim(number_format(hexdec($value["result"])/1000000000000000000, 18, ",", "."), 0), ",");
 		}
 		
-		echo "<pre>"; var_dump($account_info); echo "</pre>";
+		//echo "<pre>"; var_dump($account_info); echo "</pre>";
 
 
-		
+		echo '<div class="container"><br>';
+
+		echo '<div class="row"><div class="col-md-1">';
+		echo '</div><div class="col-md-10 text-center">';
+		echo "<h4>Account: $account </h4>";
+		echo '</div><div class="col-md-1">';
+		echo "</div></div>";
+
+		?>
+		<div class="table-responsive">
+		<table class="table table-hover">
+		<tbody>
+			<tr><td>account</td><td><?php echo $account; ?></td></tr>
+			<tr><td>earliest</td><td><?php echo $account_info["earliest"]; ?></td></tr>
+			<tr><td>latest</td><td><?php echo $account_info["latest"]; ?></td></tr>
+			<tr><td>pending</td><td><?php echo $account_info["pending"]; ?></td></tr>
+		</tbody>
+		</table>
+		</div>
+		<br></br>
+		<h4>Recent Activity</h4>
+		<?php
 
 	}
 }

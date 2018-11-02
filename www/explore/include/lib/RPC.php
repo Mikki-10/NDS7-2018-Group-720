317086e121b7de88944da4fab31b2b2d2964f769
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-02 10:33:05 +0100

    added uncle block info

--- www/explore/include/lib/RPC.php
+++ www/explore/include/lib/RPC.php
@@ -89,6 +89,7 @@ class RPC
 	function get_Uncle_By_Block_Hash_And_Index($hash, $index)
 	{
 		$index = dechex($index);
+		$block = "0x" . $block;
 		return $this->request('{"jsonrpc":"2.0","method":"eth_getUncleByBlockHashAndIndex","params":["'.$hash.'", "'.$index.'"],"id":1}');
 
 	}

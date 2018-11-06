b6a0715fe6da52b08d0c25968863d9a377abbb12
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-06 09:58:56 +0100

    save a json buffer for faster respons

--- www/explore/include/lib/TOOLS.php
+++ www/explore/include/lib/TOOLS.php
@@ -31,27 +31,49 @@ class TOOLS
 		$RPC = new RPC();
 		$block = $RPC->get_block_hight();
 
-		for ($i=0; $i < $block; $i++) 
-		{ 
-			$block_data[$i] = $RPC->get_Block($i);
+		if (file_exists("blocktime-filter.json")) 
+		{
+			$time_filter = json_decode(file_get_contents("blocktime-filter.json"), true);
+			$max = max(array_keys($time_filter));
+
+			for ($i=$max; $i < $block; $i++) 
+			{ 
+				$block_data[$i] = $RPC->get_Block($i);
+			}
+		}
+		else
+		{
+			for ($i=0; $i < $block; $i++) 
+			{ 
+				$block_data[$i] = $RPC->get_Block($i);
+			}
 		}
 
 		foreach ($block_data as $key => $block) 
 		{
 			if (array_key_exists($key+1, $block_data)) 
 			{
-				$holder[$key] = hexdec($block_data[$key+1]["result"]["timestamp"])-hexdec($block["result"]["timestamp"]);
+				if ($time_filter[$key] >= 1000) 
+				{
+					// Must be a pause in the hole blockchain (1000 sec)
+				}
+				else
+				{
+					$time_filter[$key] = hexdec($block_data[$key+1]["result"]["timestamp"])-hexdec($block["result"]["timestamp"]);
+				}
 			}
 		}
 
-		if(count($holder)) 
+		if(count($time_filter)) 
 		{
-		    $holder = array_filter($holder);
-		    $average = array_sum($holder)/count($holder);
+		    $time_filter = array_filter($time_filter);
+		    $average = array_sum($time_filter)/count($time_filter);
 		}
 
+		file_put_contents("blocktime-filter.json", json_encode($time_filter))
+
 		echo "<br>Number: " . hexdec($block["result"]["number"]);
-		echo "<br>Number (counted): " . count($holder);
+		echo "<br>Number (counted): " . count($time_filter);
 		echo "<br>Average: " . $average;
 	}
 

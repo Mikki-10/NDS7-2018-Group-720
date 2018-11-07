417deff50b16a872b95a3e493fae723da38485aa
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-07 13:59:47 +0100

    small update

--- www/explore/include/start_gui.php
+++ www/explore/include/start_gui.php
@@ -21,14 +21,14 @@
 
  <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
  <div class="container">
-  <a class="navbar-brand" href="/explore">NDS Block explore</a>
+  <a class="navbar-brand" href="/explore/">NDS Block explore</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="/explore">Home</a>
+        <a class="nav-link" href="/explore/">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="?account=all">Accounts</a>

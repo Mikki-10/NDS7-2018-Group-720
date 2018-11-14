9807e5f4a1b430d73f8c851cab75217c930a5fdd
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-11-14 10:44:16 +0100

    Batch plot.

--- tools/forkcount/batch.m
+++ tools/forkcount/batch.m
@@ -1,14 +1,18 @@
 %% convert CSV to mat
+clear
 inputFiles = dir('./input/*.csv');
 
 for file = inputFiles'
    fprintf("Converting: %s\n", file.name)
    contents = tdfread( strcat(file.folder,'/',file.name), ';');
-   contents = parseTimes(contents);
+   contents = parseTimes2(contents);
    save(strcat('./tmp/',file.name,'.mat'), 'contents');
 end
 clear
 %% Load files
+clear
+close all
+clc
 inputFiles = dir('./tmp/*.mat');
 
 timeWindow = duration(8,0,0);
@@ -17,7 +21,7 @@ for i = 1:length(inputFiles)
     data{i} = load( strcat(inputFiles(i).folder,'/',inputFiles(i).name) );
 end
 
-%dataCounter = 1;
+fig_blocktime = figure('Name', 'Block time over time');
 
 for dataCounter = 1:length(inputFiles)
 
@@ -28,8 +32,9 @@ for dataCounter = 1:length(inputFiles)
     fork_timestamps = filterLog(data{dataCounter}.contents.Message, 'forked', data{dataCounter}.contents.deltaT);
 
     % select time frame
-    mine_timeframe = mine_timestamps(mine_timestamps > (mine_timestamps(end) - timeWindow) );
-    fork_timeframe = fork_timestamps(fork_timestamps > (fork_timestamps(end) - timeWindow) );
+    endtime = data{dataCounter}.contents.deltaT(end);
+    mine_timeframe = mine_timestamps(mine_timestamps > (endtime - timeWindow) );
+    fork_timeframe = fork_timestamps(fork_timestamps > (endtime - timeWindow) );
 
     % Caluclate time between each mined block
     block_time = mine_timeframe(2:end) - mine_timeframe(1:end-1);
@@ -43,4 +48,19 @@ for dataCounter = 1:length(inputFiles)
 
     fprintf("Fork count: %d, Mined count: %d, Fork chance: %.2f%%, Mean block time: %.2f\n\n", fork_count, mined_count, fork_chance, mean(seconds(block_time)))
 
+    % Plot block time over time.
+    figure(fig_blocktime)
+    subplot(length(inputFiles),1,dataCounter)
+    plot(mine_timeframe(1:end-1), block_time, 'DisplayName', 'Block time')
+    hold on
+    % Plot fork indicators
+    stem([fork_timeframe fork_timeframe], repmat(ylim, 1, length(fork_timeframe)), 'MarkerFaceColor', 'none', 'MarkerEdgeColor', 'none', 'LineWidth', 1);
+    hold off
+    
+    xlabel('Runtime')
+    ylabel('Block time')
+    ylim([duration(0, 0, 0) duration(0, 2, 0)])
+    legend
+    title( ['Block time - ' inputFiles(dataCounter).name] )
+    
 end

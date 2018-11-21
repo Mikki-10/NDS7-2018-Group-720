e7f33d60e7a9c85d089da21aa6d603c584f55da7
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-21 12:57:58 +0100

    adding datapoints to graph

--- www/explore/include/lib/TOOLS.php
+++ www/explore/include/lib/TOOLS.php
@@ -126,19 +126,20 @@ class TOOLS
 		<div id="container<?php echo $a_random_int;?>" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 
 		<script type="text/javascript">
-			/*
-			var data;
-			$.getJSON("http://192.168.20.3/explore/chart.json", function(json){
-			    data = json;
-			});
-			var data2;
-			$.getJSON("http://192.168.20.3/explore/chart.json", function(json){
-			    data2 = json;
-			});
-			*/
 
+                var data = $.getJSON("http://192.168.20.3/explore/chart.json", function(json) {
+                    return json;
+                });
+                var data2 = $.getJSON("http://192.168.20.3/explore/chart.json", function(json) {
+                    return json;
+                });
+
+                //console.log(data);
+                //console.log(data2);
+
+                /*
 			$.getJSON("http://192.168.20.3/explore/chart.json", function(data){
-			    .getJSON("http://192.168.20.3/explore/chart.json", function(data2){
+			    getJSON("http://192.168.20.3/explore/chart.json", function(data2){
 			    console.log(data);
 			    console.log(data2);
 				});
@@ -150,78 +151,73 @@ class TOOLS
 
 			});
 			*/
+                $.when(data, data2).done(function(data, data2) {
+                    //console.log(data[2]["responseJSON"]);
+                    //console.log(data2[2]);
+                    Highcharts.chart('container<?php echo $a_random_int;?>', {
+                        chart: {
+                            zoomType: 'x'
+                        },
+                        title: {
+                            text: 'Time between blocks'
+                        },
+                        subtitle: {
+                            text: document.ontouchstart === undefined ? 'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+                        },
+                        xAxis: {
+                            type: 'datetime'
+                        },
+                        yAxis: [{
+                            title: {
+                                text: 'Sec'
+                            },
+                            min: 0
+                        }, {
+                            title: {
+                                text: 'Dif'
+                            },
+                            opposite: true,
+                            min: 0
+                        }],
+                        legend: {
+                            enabled: false
+                        },
+                        plotOptions: {
+                            area: {
+                                fillColor: {
+                                    linearGradient: {
+                                        x1: 0,
+                                        y1: 0,
+                                        x2: 0,
+                                        y2: 1
+                                    },
+                                    stops: [[0, Highcharts.getOptions().colors[0]], [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]]
+                                },
+                                marker: {
+                                    radius: 2
+                                },
+                                lineWidth: 1,
+                                states: {
+                                    hover: {
+                                        lineWidth: 1
+                                    }
+                                },
+                                threshold: null
+                            }
+                        },
 
-		    $(function (data, data2) {
-		        
-		        Highcharts.chart('container<?php echo $a_random_int;?>', {
-		            chart: {
-		                zoomType: 'x'
-		            },
-		            title: {
-		                text: 'Time between blocks'
-		            },
-		            subtitle: {
-		                text: document.ontouchstart === undefined ?
-		                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-		            },
-		            xAxis: {
-		                type: 'datetime'
-		            },
-		            yAxis: [{
-		                title: {
-		                    text: 'Sec'
-		                },
-		                min: 0
-		            }, {
-		                title: {
-		                    text: 'Dif'
-		                },
-		                opposite: true,
-		                min: 0
-		            }],
-		            legend: {
-		                enabled: false
-		            },
-		            plotOptions: {
-		                area: {
-		                    fillColor: {
-		                        linearGradient: {
-		                            x1: 0,
-		                            y1: 0,
-		                            x2: 0,
-		                            y2: 1
-		                        },
-		                        stops: [
-		                            [0, Highcharts.getOptions().colors[0]],
-		                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-		                        ]
-		                    },
-		                    marker: {
-		                        radius: 2
-		                    },
-		                    lineWidth: 1,
-		                    states: {
-		                        hover: {
-		                            lineWidth: 1
-		                        }
-		                    },
-		                    threshold: null
-		                }
-		            },
-
-		            series: [{
-		                type: 'area',
-		                name: 'Time between blocks',
-		                data: data
-		            }, {
-		                type: 'area',
-		                name: 'Time between blocks',
-		                data: data2
-		            }]
-		        });
-		    }
-		    );
-		</script>
+                        series: [{
+                            type: 'area',
+                            name: 'Time between blocks',
+                            data: data[2]["responseJSON"]
+                        }, {
+                            type: 'area',
+                            name: 'Time between blocks',
+                            data: data2[2]["responseJSON"]
+                        }]
+                    });
+                });
+            </script>
 
 		<?php
 

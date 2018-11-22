48e39f7fd0e0442332f458d190ea1cf50c404f2b
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-22 13:23:16 +0100

    adding datapoints to graph

--- www/topology.php
+++ www/topology.php
@@ -317,6 +317,19 @@ fill:#555;
 
 <script>
 
+var width = 1400,
+    height = 850
+
+var svg = d3.select("graph-container").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var force = d3.layout.force()
+    .gravity(.5)
+    .distance(400)
+    .charge(-100)
+    .size([width, height]);
+
 var inter = setInterval(function() {
 				d3.selectAll('.graph-container svg').remove();
 				

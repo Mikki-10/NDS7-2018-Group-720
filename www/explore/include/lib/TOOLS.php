b9d525f59b90d3402a2b33134ddc11c154f79170
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-27 13:25:11 +0100

    client side export

--- www/explore/include/lib/TOOLS.php
+++ www/explore/include/lib/TOOLS.php
@@ -132,7 +132,6 @@ class TOOLS
 		<script src="https://code.highcharts.com/highcharts.js"></script>
 		<script src="https://code.highcharts.com/modules/exporting.js"></script>
 		<script src="https://code.highcharts.com/modules/export-data.js"></script>
-		<script src="https://code.highcharts.com/highcharts.js"></script>
 		<script src="https://code.highcharts.com/modules/offline-exporting.js"></script>
 
 		<?php

2d9ef23087183da201a1d0d6d7cac8cb6fd9d82d
Mikki Sørensen <Mikki-10@hotmail.com>
2018-11-28 12:40:54 +0100

    making a cleaning option, to clear json files that are used for caching

--- www/explore/include/lib/CLEAN.php
+++ www/explore/include/lib/CLEAN.php
@@ -15,7 +15,7 @@ class CLEAN
 	{
 		$files = scandir(dirname(__FILE__));
 
-		echo "<pre>"; var_dump(dirname(__FILE__)); echo "</pre>";
+		echo "<pre>"; var_dump("../../" . dirname(__FILE__)); echo "</pre>";
 		echo "<pre>"; var_dump($files); echo "</pre>";
 
 		foreach ($files as $key => $filename) 

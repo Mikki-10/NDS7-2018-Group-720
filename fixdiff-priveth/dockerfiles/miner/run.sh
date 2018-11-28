d22b75724c9df1b3bc623d0e1c34a6691241e048
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-11-28 11:13:28 +0100

    wefwef

--- fixdiff-priveth/dockerfiles/miner/run.sh
+++ fixdiff-priveth/dockerfiles/miner/run.sh
@@ -43,7 +43,7 @@ then
         --networkid=44686 \
         --rpccorsdomain "http://$(hostname -i):8000" \
         --verbosity $VERBOSITY \
-        --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@bootnode:30301 \
+        --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.19.0.2:30301 \
         --mine \
         --minerthreads=1 \
         --etherbase=$accountNumber \
@@ -60,7 +60,7 @@ else
         --rpccorsdomain "http://$(hostname -i):8000" \
         --networkid=44686 \
         --verbosity 4 \
-        --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@bootnode:30301 \
+        --bootnodes=enode://a93cc3de8693e2ad879df9b3c306c1b9752b49d1550615825e5049528c8b109b5dbdf9847f1b6cae463f4321118b6126c673890c7ad2f706c57b466bbcf66a08@172.19.0.2:30301 \
         --mine \
         --minerthreads=1 \
         --etherbase=$accountNumber

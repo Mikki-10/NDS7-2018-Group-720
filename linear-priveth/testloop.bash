85c70a424672a7499060354499ef8b995efe9cb4
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2018-12-03 12:17:36 +0100

    set up testloop for linear test

--- linear-priveth/testloop.bash
+++ linear-priveth/testloop.bash
@@ -8,22 +8,29 @@ setDelay() {
 
     for c in $containers
     do
-            docker exec -d $c tc qdisc change dev eth0 root netem delay ${DELAY}ms loss ${LOSS}%
+            docker exec $c tc qdisc change dev eth0 root netem delay ${DELAY}ms loss ${LOSS}%
     done
 }
 
-tar czf 0-startup-logs.tar.gz ./logs
-
-for i in {1..10}
+for repeat in {1..3}
 do
+    echo "Beginning repeat " $repeat
+    tar czf 0-startup-logs.tar.gz ./logs
 
-    delay_time=$((25*$i))
-    loss_pct=0
-    echo "Beginning test: $i with delay: $delay_time and loss: $loss_pct"
-    setDelay $delay_time $loss_pct
+    for i in {1..2}
+    do
+        echo "Beginning test " $i
+        delay_time=$((400*($i-1)))
+        loss_pct=0
+        echo "Beginning test: $i with delay: $delay_time and loss: $loss_pct"
+        setDelay $delay_time $loss_pct
 
-    sleep 1h
+        sleep 5h
 
-    tar czf ${i}-delay${delay_time}-loss${loss_pct}-logs.tar.gz ./logs
+        tar czf ${i}-line-delay${delay_time}-loss${loss_pct}-logs.tar.gz ./logs
+        echo "End of teset " $i
+    done
 
+    mkdir $repeat-test-run
+    mv *.tar.gz $repeat-test-run
 done

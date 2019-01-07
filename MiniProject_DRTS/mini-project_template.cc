b523509a8c8df3c58668c969cc3caaf2de6b9dbe
Flavien Marty <flavienmarty@MacBook-Pro-de-Flavien.local>
2019-01-07 07:12:22 +0100

    rate 3mbps

--- MiniProject_DRTS/mini-project_template.cc
+++ MiniProject_DRTS/mini-project_template.cc
@@ -165,7 +165,7 @@ main (int argc, char *argv[])
   uint32_t burst = 10000;//tokenbucket burst parameter (original value 10000)
   uint32_t mtu = 0;
   QueueSize maxS = QueueSize("8p");
-  DataRate rate = DataRate ("5Mbps"); //original value 48Mbps
+  DataRate rate = DataRate ("3Mbps"); //original value 48Mbps
   DataRate peakRate = 0;
 
   TrafficControlHelper tch;

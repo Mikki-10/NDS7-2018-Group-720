ecc25d4d9640ce915ec68886a0f1807c0de76f2e
Flavien Marty <flavienmarty@h235.natout.aau.dk>
2019-01-08 10:20:22 +0100

    set back to 4p

--- MiniProject_DRTS/mini-project_template.cc
+++ MiniProject_DRTS/mini-project_template.cc
@@ -164,8 +164,8 @@ main (int argc, char *argv[])
   // Configurations
   uint32_t burst = 10000;//tokenbucket burst parameter (original value 10000)
   uint32_t mtu = 0;
-  QueueSize maxS = QueueSize("8p");
-  DataRate rate = DataRate ("4Mbps"); //original value 48Mbps
+  QueueSize maxS = QueueSize("4p");
+  DataRate rate = DataRate ("48Mbps"); //original value 48Mbps
   DataRate peakRate = 0;
 
   TrafficControlHelper tch;
